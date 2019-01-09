670bfca03a9307e650f6a0f8ca3a2693086f37d2
ronmelcuba10 <roncareer76@yahoo.com>
2019-01-09 10:50:36 -0500

    initial

---
+++ events.bat
@@ -0,0 +1,2 @@
+SQLCMD -S .\SQLEXPRESS -U user -P password -i sql_file.sql -h -1 > auto_gerenerated_output_file.txt
+EXIT
---
+++ sql_file.sql
@@ -0,0 +1,10 @@
+USE SchedulerProduction;
+
+PRINT 'Printing a List'
+EXEC dbo.Getlist;
+
+PRINT 'Running a SP'
+EXEC dbo.MyStoredProcedure;
+
+GO
+
